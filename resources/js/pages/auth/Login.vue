c50c778eea5161f7a9032b585ec0cbe87d335a01
Eduardo Elizondo <eduardo.elizondof@gmail.com>
2025-12-08 09:13:45 -0600

    Traducciones

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -20,8 +20,8 @@ defineProps<{
 
 <template>
     <AuthBase
-        title="Log in to your account"
-        description="Enter your email and password below to log in"
+        title="Inicia sesión en tu cuenta"
+        description="Ingresa tu correo electrónico y contraseña para iniciar sesión"
     >
         <Head title="Log in" />
 
@@ -40,7 +40,7 @@ defineProps<{
         >
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
+                    <Label for="email">Correo electrónico</Label>
                     <Input
                         id="email"
                         type="email"
@@ -56,14 +56,14 @@ defineProps<{
 
                 <div class="grid gap-2">
                     <div class="flex items-center justify-between">
-                        <Label for="password">Password</Label>
+                        <Label for="password">Contraseña</Label>
                         <TextLink
                             v-if="canResetPassword"
                             :href="request()"
                             class="text-sm"
                             :tabindex="5"
                         >
-                            Forgot password?
+                            Olvidaste tu contraseña?
                         </TextLink>
                     </div>
                     <Input
@@ -73,7 +73,7 @@ defineProps<{
                         required
                         :tabindex="2"
                         autocomplete="current-password"
-                        placeholder="Password"
+                        placeholder="Contraseña"
                     />
                     <InputError :message="errors.password" />
                 </div>
@@ -81,7 +81,7 @@ defineProps<{
                 <div class="flex items-center justify-between">
                     <Label for="remember" class="flex items-center space-x-3">
                         <Checkbox id="remember" name="remember" :tabindex="3" />
-                        <span>Remember me</span>
+                        <span>Recuérdame</span>
                     </Label>
                 </div>
 
@@ -93,7 +93,7 @@ defineProps<{
                     data-test="login-button"
                 >
                     <Spinner v-if="processing" />
-                    Log in
+                    Iniciar sesión
                 </Button>
             </div>
 
